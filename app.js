8c271e9df22f2a61b7a410502ccf69cc6b434d9a
TheGameGamerInt <snorre@forbregd.com>
2021-10-19 10:25:47 +0200

    Adding Express and formating

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var express = require('express')
+var app = express()
+var port = 5000
 var mysql = require('mysql');
 var config = require('./config.json');
+
+app.use(express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('', (req, res) => {
+  res.render('index', { text: 'Hey' })
+})
+
+app.get('/about', (req, res) => {
+ res.sendFile(__dirname + '/views/about.html')
+})
+
+app.listen(port, () => console.log(port))
 
 var con = mysql.createConnection({
   host: "localhost",
