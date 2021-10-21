1aa09a38d543f74bc4fbddd744885b7757e09ea5
TheGameGamerInt <snorre@forbregd.com>
2021-10-21 13:53:14 +0200

    Create Test branch for teacher to look around in

--- app.js
+++ app.js
@@ -6,33 +6,51 @@ var app = express()
 var port = 5000
 var mysql = require('mysql');
 var config = require('./config.json');
-const path = require('path');
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "Snorre",
+  password: config.PASSWORD,
+  database: "nodesite"
+});
+con.connect(function (err) {
+  if (err) throw err;
+  con.query("SHOW TABLES",function (err, result) {
+    if (err) throw err;
+    console.log(Object.getOwnPropertyNames(result))
+      console.log((result[1]))
+  });
+});
+
+function insert(table, columns, values) {
+  if(Array.isArray(values[0])) {
+    values.forEach(val => {
+      con.query(`INSERT INTO ${table} (${columns.toString()}) VALUES (${val.toString()})`)
+    });
+  } else {
+    con.query(`INSERT INTO ${table} (${columns.toString()}) VALUES (${values.toString()})`)
+  }
+}
+
 
 app.use(express.static('public'));
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "Snorre",
-  password: config.PASSWORD
-});
-
 app.get('', (req, res) => {
-  res.render('index', { text: url.parse(req.url, true).pathname})
+  res.render('index', {
+    text: url.parse(req.url, true).pathname
+  })
 })
 
 app.get('/about', (req, res) => {
- res.render('about')
+  res.render('about')
 })
 app.get('*', (req, res) => {
   res.redirect('/')
- })
- 
+})
+
 app.listen(port, () => console.log(port))
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
